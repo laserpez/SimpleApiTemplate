09e867d52326e0d5944a088d18fbf06f0adfe94b
Simone Finotti <simone.finotti.external@ynap.com>
2018-03-01 17:38:00 +0100

    General update.

--- src/SimpleApiConsoleHost/Program.cs
+++ src/SimpleApiConsoleHost/Program.cs
@@ -1,14 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Microsoft.Owin.Hosting;
 using ProjectAPI;
 
 namespace ConsoleHostProject
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
